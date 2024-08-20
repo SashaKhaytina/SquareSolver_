ff90b6dec386da296d1bcb6faf31a448aa2ebc9e
SashaKhaytina <sasha-khaytina@rambler.ru>
2024-08-20 19:38:21 +0300

    Update square.cpp

--- square.cpp
+++ square.cpp
@@ -16,8 +16,8 @@ RootsNumber solve_square(double a, double b, double c, double* x1, double* x2);
 void input_coeff(double* coeff, char symbol);
 void output(int n_answer, double* x1, double* x2);
 bool is_null(double n);
-void go_to_new_str();
-
+void clear_boofer();
+RootsNumber solve_line(double k, double b, double* x1);
 
 
 struct SquareEquation {
@@ -35,13 +35,13 @@ int main()
     // ax^2 + bx + c = 0
 
 
-    struct my_square equation = {};
+    struct SquareEquation equation = {};
 
     input_coeff(&equation.a, 'a');
     input_coeff(&equation.b, 'b');
     input_coeff(&equation.c, 'c');
 
-    int n_answer = solve(equation.a, equation.b, equation.c, &equation.x1, &equation.x2);
+    int n_answer = solve_square(equation.a, equation.b, equation.c, &equation.x1, &equation.x2);
 
     output(n_answer, &equation.x1, &equation.x2);
 }
@@ -58,7 +58,7 @@ void input_coeff(double* coeff, char symbol)
 
     int flag = scanf("%lf", coeff);
     while(flag == 0){
-        go_to_new_str();
+        clear_boofer();
         printf("Ошибка. Введите коэффициент %c еще раз:\n", symbol);
         flag = scanf("%lf", coeff);
 
@@ -70,23 +70,16 @@ void input_coeff(double* coeff, char symbol)
 
 
 
-RootsNumber solve(double a, double b, double c, double* x1, double* x2)
+RootsNumber solve_square(double a, double b, double c, double* x1, double* x2)
 {
     // сравнение чисел с точкой!
     if (is_null(a)){
-        if (is_null(b)){
-            if (is_null(c)) return INF;
-            else return NULL_ANS;
-        }
-        else{
-            *x1 = -c/b;
-            return ONE_ANS;
-        }
+        return solve_line(b, c, x1);
     }
     else{
         double disc = b * b - 4 * a *c;
         if (disc < 0) return NULL_ANS;
-        if ((disc - 0) < DELTA){
+        if (is_null(disc)){
             *x1 = -b/(2*a);
             return ONE_ANS;
         }
@@ -143,7 +136,24 @@ bool is_null(double n)
 
 
 
-void go_to_new_str()
+void clear_boofer()
 {
     while(getchar() != '\n') continue;
+}
+
+
+
+
+
+RootsNumber solve_line(double k, double b, double* x1)
+{
+    if (is_null(k)){
+            if (is_null(b)) return INF;
+            else return NULL_ANS;
+        }
+        else{
+            *x1 = -b/k;
+            return ONE_ANS;
+        }
+
 }
