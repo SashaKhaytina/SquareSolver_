5806577571480449b64810d2bf0736f5bccd2a8b
SashaKhaytina <sasha-khaytina@rambler.ru>
2024-08-21 22:29:31 +0300

    Update square.cpp

--- square.cpp
+++ square.cpp
@@ -4,74 +4,10 @@
 
 #include "major.h"
 
-#define DEBUG_PRINT printf ("%d\n", __LINE__);
+#define DEBUG_PRINT //printf ("%d\n", __LINE__);
 
-#define TEST
+//#define TEST
 
-/*
-#define fun() \
-    printf("hey");
-
-
-const double DELTA = 1e-7;
-
-enum RootsNumber 
-{
-    INF_ROOTS,
-    ZERO_ROOTS,
-    ONE_ROOTS,
-    TWO_ROOTS
-};
-
-enum CheckTest 
-{
-    TEST_ERROR,
-    TEST_OK
-};
-
-enum AnyErrors
-{
-    A_IS_NOT_NUM,
-    B_IS_NOT_NUM,
-    C_IS_NOT_NUM,
-    ALL_OK
-};
-
-struct SquareEquation 
-{
-    double a;
-    double b;
-    double c;
-    double x1;
-    double x2;
-    RootsNumber roots_num;
-};
-
-AnyErrors solve_square(SquareEquation* equation);
-void input_coeff(double* coeff, char symbol);
-void output_roots(SquareEquation* equation);
-bool is_null(double n);
-bool is_equally(double a, double b);
-void clear_buffer();
-AnyErrors solve_linear(double k, double b, double* x1, RootsNumber* roots_num);  // kx + b = 0
-void run_all_tests();
-CheckTest run_test(const SquareEquation* expected, double* t_x1, double* t_x2, RootsNumber* t_roots_num);
-void print_test_result(const SquareEquation* expected, double t_x1, double t_x2, RootsNumber t_roots_num, int test_number, CheckTest checker);
-*/
-
-//#include "Tester.cpp"
-
-//void print_test_result(CheckTest checker, int test_number);
-/*
-const struct SquareEquation TESTS[] = {
-    {0, 0, 0, 0, 0, INF_ROOTS}, 
-    {1, 3, 2, -1, -2, TWO_ROOTS}, 
-    {0, 7, 0, 0, 0, ONE_ROOTS}, 
-    {1, 1, 1, 0, 0, ZERO_ROOTS},
-    {0, 7, 0, 5, 5, TWO_ROOTS},
-    {7, 0, 7, 0, 0, ZERO_ROOTS}
-};
-*/
 
 int main()
 {
@@ -102,52 +38,77 @@ int main()
 
 bool input_coeffs(SquareEquation* equation)
 {
-    if (!input_coeff(&(equation->a), 'a'))
-    {
-        return false;
-    }
-    
-    if (!input_coeff(&(equation->b), 'b'))
-    {
-        return false;
-    }
+    assert(equation);
 
-    if (!input_coeff(&(equation->c), 'c'))
-    {
-        return false;
-    }
+    input_coeff(&(equation->a), 'a');
+    input_coeff(&(equation->b), 'b');
+    input_coeff(&(equation->c), 'c');
+
+    // if (!input_coeff(&(equation->a), 'a'))
+    // {
+    //     return false;
+    // }
+    
+    // if (!input_coeff(&(equation->b), 'b'))
+    // {
+    //     return false;
+    // }
+
+    // if (!input_coeff(&(equation->c), 'c'))
+    // {
+    //     return false;
+    // }
 
     return true;
 }
 
-bool input_coeff(double* coeff, char symbol)
+// bool input_coeff(double* coeff, char symbol)
+// {
+    // assert(coeff);
+
+    // printf("Введите коэффициент %c\n%c = ", symbol, symbol);
+    // scanf("%lf", coeff);
+    // DEBUG_PRINT
+    // char c = getchar();
+    // DEBUG_PRINT
+
+    // while (c != '\n' && c != '\t' && c != ' ') 
+    // {
+    //     if (c == EOF)
+    //     {
+    //         return false;
+    //     }
+    //     DEBUG_PRINT
+    //     clear_buffer();
+    //     DEBUG_PRINT
+    //     printf("Ошибка. Введите коэффициент %c еще раз:\n", symbol);
+    //     DEBUG_PRINT
+    //     scanf("%lf", coeff);
+    //     c = getchar();
+
+
+    // }
+    // return true;
+
+void input_coeff(double* coeff, char symbol)
 {
     assert(coeff);
 
     printf("Введите коэффициент %c\n%c = ", symbol, symbol);
-    scanf("%lf", coeff);
-    DEBUG_PRINT;
-    char c = getchar();
     DEBUG_PRINT
-
-    while (c != '\n' && c != '\t' && c != ' ') 
+    // while ((scanf("%lf", coeff) != 1) && (getchar() != '\n'))
+    while (scanf("%lf", coeff) != 1) 
     {
-        if (c == EOF)
-        {
-            return false;
-        }
         DEBUG_PRINT
         clear_buffer();
         DEBUG_PRINT
         printf("Ошибка. Введите коэффициент %c еще раз:\n", symbol);
-        DEBUG_PRINT
-        scanf("%lf", coeff);
-        c = getchar();
-
-
     }
-    return true;
+
+    clear_buffer();
+
 }
+
 
 
 AnyErrors solve_square(SquareEquation* equation) 
@@ -166,7 +127,6 @@ AnyErrors solve_square(SquareEquation* equation)
 
     if (is_null(a))
     {
-        //equation->roots_num = solve_linear(b, c, x1);
         solve_linear(b, c, x1, &(equation->roots_num)); ////////////////////// Куда-то сохранять ошибку? ///////////////////////////////////
     }
     else
@@ -234,8 +194,7 @@ bool is_equally(double a, double b)
 
 void clear_buffer()
 {
-    char c = getchar();
-    while (c != '\n' && c != EOF) continue;
+    while (getchar() != '\n') continue;
 }
 
 
@@ -259,61 +218,3 @@ AnyErrors solve_linear(double k, double b, double* x1, RootsNumber* roots_num)
     }
     return ALL_OK;
 }
-
-/*
-void print_test_result(const SquareEquation* expected, double t_x1, double t_x2, RootsNumber t_roots_num, int test_number, CheckTest checker)
-{
-    assert(expected);
-
-    if (checker) printf("Test %d - OK\n", test_number + 1);
-    else
-    {
-        printf("Test %d - ERROR\nError test: x1 = %lg, x2 = %lg, nRoots = %d\n"
-           "Expected: x1 = %lg, x2 = %lg, nRoots = %d\n", 
-           test_number, t_x1, t_x2, t_roots_num, expected->x1, expected->x2, expected->roots_num);
-    }
-
-}
-
-CheckTest run_test(const SquareEquation* expected, double* t_x1, double* t_x2, RootsNumber* t_roots_num)
-{
-    assert(expected);
-    assert(t_x1);
-    assert(t_x2);
-    assert(t_roots_num);
-
-    SquareEquation test = {expected->a, expected->b, expected->c, 0, 0};
-    solve_square(&test);
-
-    bool check_x1 = is_equally(test.x1, expected->x1);
-    bool check_x2 = is_equally(test.x2, expected->x2);
-    bool check_roots_num = is_equally(test.roots_num, expected->roots_num);
-
-    *t_x1 = test.x1;
-    *t_x2 = test.x2;
-    *t_roots_num = test.roots_num;
-
-    if (check_x1 && check_x2 && check_roots_num) return TEST_OK;
-    else
-    {
-        //error_test(expected, test.x1, test.x2, test.roots_num);
-        return TEST_ERROR;
-    }
-}
-
-void run_all_tests()
-{
-    int quantity_tests = sizeof(TESTS) / sizeof(SquareEquation);
-
-    for (int test_number = 0; test_number < quantity_tests; test_number++)
-    {
-        double t_x1 = 0, t_x2 = 0;
-        RootsNumber t_roots_num = ZERO_ROOTS;
-
-        const SquareEquation* test_i = &(TESTS[test_number]);
-        CheckTest checker = run_test(test_i, &t_x1, &t_x2, &t_roots_num);
-        print_test_result(test_i, t_x1, t_x2, t_roots_num, test_number, checker);
-
-    }
-}
-*/
